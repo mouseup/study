a73fcb51b3683f20fd5805cbf401ee14513d829b
mouseup <mouseup@hanmail.net>
2019-11-21 09:00:04 +0900

    DoIt10

---
+++ android/SampleActionBar1/app/src/main/java/org/techtown/sampleactionbar1/MainActivity.java
@@ -0,0 +1,65 @@
+package org.techtown.sampleactionbar1;
+
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ActionBar abar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // xml 레이아웃에 들어 있는 ActionBar 객체 참조
+        abar = getSupportActionBar();
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abar.setLogo(R.drawable.home);
+                // setDisplayOptions() - 버튼을 클릭했을때 액션바가 보이는 모양을 바꿈
+                // DISPLAY_SHOW_HOME - 홈 아이콘을 표시
+                // DISPLAY_USE_LOGO - 홈 아이콘 부분에 로고 아이콘 사용
+                abar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME|ActionBar.DISPLAY_USE_LOGO);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int curId = item.getItemId();
+        switch (curId) {
+            case R.id.menu_refresh:
+                Toast.makeText(this, "새로고침 메뉴가 선택되었습니다.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_search:
+                Toast.makeText(this, "검색 메뉴가 선택되었습니다.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_settings:
+                Toast.makeText(this, "설정 메뉴가 선택되었습니다.", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
