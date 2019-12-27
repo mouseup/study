14b061f507ece343547a02446a26f3fa149fc068
mouseup <mouseup@hanmail.net>
2019-12-27 16:47:20 +0900

    Mission21

---
+++ android_Mission/DoItMission21/app/src/main/java/org/techtown/mission21/BookDatabase.java
@@ -0,0 +1,206 @@
+package org.techtown.mission21;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class BookDatabase {
+	private static final String TAG = "BookDatabase";
+
+	/**
+	 * Singleton instance
+	 */
+	private static BookDatabase database;
+
+
+	/**
+	 * database name
+	 */
+	public static String DATABASE_NAME = "book.db";
+
+	/**
+	 * table name for BOOK_INFO
+	 */
+	public static String TABLE_BOOK_INFO = "BOOK_INFO";
+
+    /**
+     * version
+     */
+	public static int DATABASE_VERSION = 1;
+
+
+    /**
+     * Helper class defined
+     */
+    private DatabaseHelper dbHelper;
+
+    /**
+     * Database object
+     */
+    private SQLiteDatabase db;
+
+
+    private Context context;
+
+    /**
+     * Constructor
+     */
+	private BookDatabase(Context context) {
+		this.context = context;
+	}
+
+
+	public static BookDatabase getInstance(Context context) {
+		if (database == null) {
+			database = new BookDatabase(context);
+		}
+
+		return database;
+	}
+
+	/**
+	 * open database
+	 */
+    public boolean open() {
+    	println("opening database [" + DATABASE_NAME + "].");
+
+    	// DatabaseHelper 객체 생성하고 SQLiteDatabase 객체 참조
+    	dbHelper = new DatabaseHelper(context);
+    	db = dbHelper.getWritableDatabase();
+
+    	return true;
+    }
+
+    /**
+     * close database
+     */
+    public void close() {
+    	println("closing database [" + DATABASE_NAME + "].");
+    	db.close();
+    	database = null;
+    }
+
+    /**
+     * execute raw query using the input SQL
+     * close the cursor after fetching any result
+     */
+    public Cursor rawQuery(String SQL) {
+		println("\nexecuteQuery called.\n");
+
+		Cursor c1 = null;
+		try {
+			c1 = db.rawQuery(SQL, null);
+			println("cursor count : " + c1.getCount());
+		} catch(Exception ex) {
+    		Log.e(TAG, "Exception in executeQuery", ex);
+    	}
+
+		return c1;
+	}
+
+    public boolean execSQL(String SQL) {
+		println("\nexecute called.\n");
+
+		try {
+			Log.d(TAG, "SQL : " + SQL);
+			db.execSQL(SQL);
+	    } catch(Exception ex) {
+			Log.e(TAG, "Exception in executeQuery", ex);
+			return false;
+		}
+
+		return true;
+	}
+
+
+
+
+	// SQLiteOpenHelper 클래스를 상속하여 새로운 클래스 정의
+    private class DatabaseHelper extends SQLiteOpenHelper {
+
+        public DatabaseHelper(Context context) {
+
+        	// 데이타베이스 이름과 버전 정보로 상위 클래스 생성자 호출
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        // 데이타베이스 생성
+        public void onCreate(SQLiteDatabase _db) {
+        	// TABLE_BOOK_INFO
+        	println("creating table [" + TABLE_BOOK_INFO + "].");
+
+        	// drop existing table
+        	String DROP_SQL = "drop table if exists " + TABLE_BOOK_INFO;
+        	try {
+        		_db.execSQL(DROP_SQL);
+        	} catch(Exception ex) {
+        		Log.e(TAG, "Exception in DROP_SQL", ex);
+        	}
+
+        	// create table
+        	String CREATE_SQL = "create table " + TABLE_BOOK_INFO + "("
+		        			+ "  _id INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT, "
+		        			+ "  NAME TEXT, "
+		        			+ "  AUTHOR TEXT, "
+		        			+ "  CONTENTS TEXT, "
+		        			+ "  CREATE_DATE TIMESTAMP DEFAULT CURRENT_TIMESTAMP "
+		        			+ ")";
+            try {
+
+            	// sql 실행
+            	_db.execSQL(CREATE_SQL);
+            } catch(Exception ex) {
+        		Log.e(TAG, "Exception in CREATE_SQL", ex);
+        	}
+
+			// insert 5 book records
+			insertRecord(_db, "Do it! 안드로이드 앱 프로그래밍", "정재곤", "안드로이드 기본서로 이지스퍼블리싱 출판사에서 출판했습니다.");
+			insertRecord(_db, "Programming Android", "Mednieks, Zigurd", "Oreilly Associates Inc에서 출판했습니다.");
+			insertRecord(_db, "모바일 프로그래밍", "이병옥,최성민 공저", "에이콘출판사에서 출판했습니다.");
+			insertRecord(_db, "시작하세요! 안드로이드 게임 프로그래밍", "마리오 제흐너 저", "위키북스에서 출판했습니다.");
+			insertRecord(_db, "실전! 안드로이드 시스템 프로그래밍 완전정복", "박선호,오영환 공저", "DW Wave에서 출판했습니다.");
+
+		}
+
+		// 데이터베이스 열때
+        public void onOpen(SQLiteDatabase db) {
+        	println("opened database [" + DATABASE_NAME + "].");
+
+        }
+
+        // 데이타베이스 업그레이드
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        	println("Upgrading database from version " + oldVersion + " to " + newVersion + ".");
+
+        	if (oldVersion < 2) {   // version 1
+
+        	}
+
+        }
+
+		private void insertRecord(SQLiteDatabase _db, String name, String author, String contents) {
+			try {
+				_db.execSQL( "insert into " + TABLE_BOOK_INFO + "(NAME, AUTHOR, CONTENTS) values ('" + name + "', '" + author + "', '" + contents + "');" );
+			} catch(Exception ex) {
+				Log.e(TAG, "Exception in executing insert SQL.", ex);
+			}
+		}
+
+    }
+
+	public void insertRecord(String name, String author, String contents) {
+		try {
+			db.execSQL( "insert into " + TABLE_BOOK_INFO + "(NAME, AUTHOR, CONTENTS) values ('" + name + "', '" + author + "', '" + contents + "');" );
+		} catch(Exception ex) {
+			Log.e(TAG, "Exception in executing insert SQL.", ex);
+		}
+	}
+
+    private void println(String msg) {
+    	Log.d(TAG, msg);
+    }
+
+
+}
