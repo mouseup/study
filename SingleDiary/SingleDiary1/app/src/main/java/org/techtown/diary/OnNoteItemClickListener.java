ef53df35bc6dcc9a7b0cb55a5e7542923fa9bcc0
mouseup <mouseup@hanmail.net>
2020-01-08 19:45:35 +0900

    SingleDiary

--- SingleDiary/SingleDiary1/app/src/main/java/org/techtown/diary/OnNoteItemClickListener.java
+++
@@ -1,8 +0,0 @@
-package org.techtown.diary;
-
-import android.view.View;
-
-// 아이템 선택시 이벤트 처리
-public interface OnNoteItemClickListener {
-    public void onItemClick(NoteAdapter.ViewHolder holder, View view, int position);
-}
--- SingleDiary/SingleDiary1/app/src/main/java/org/techtown/diary/OnTabItemSelectedListener.java
+++
@@ -1,8 +0,0 @@
-package org.techtown.diary;
-
-import android.view.View;
-
-// 하나의 프래그먼트에서 다른 프래그먼트로 전환
-public interface OnTabItemSelectedListener {
-    public void onTabSelected(int position);
-}
