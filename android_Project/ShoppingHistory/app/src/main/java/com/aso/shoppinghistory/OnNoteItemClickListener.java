d197042f08a1b6c8d00f40157293466e00e46949
mouseup <mouseup@hanmail.net>
2020-01-14 15:11:21 +0900

    ShoppingHistory1

---
+++ android_Project/ShoppingHistory/app/src/main/java/com/aso/shoppinghistory/OnNoteItemClickListener.java
@@ -0,0 +1,7 @@
+package com.aso.shoppinghistory;
+
+import android.view.View;
+
+public interface OnNoteItemClickListener {
+    public void onItemClick(NoteAdapter.ViewHolder holder, View view, int position);
+}
---
+++ android_Project/ShoppingHistory/app/src/main/java/com/aso/shoppinghistory/OnTabItemSelectedListener.java
@@ -0,0 +1,5 @@
+package com.aso.shoppinghistory;
+
+public interface OnTabItemSelectedListener {
+    public void onTabSelected(int position);
+}
